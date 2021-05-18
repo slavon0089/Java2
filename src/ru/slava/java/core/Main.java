7b746833d62d0082ed9c6c4066f53e34bc0a74da
slavon0089 <slavon0089@yandex.ru>
2021-05-19 01:12:07 +0300

    Home work 2

--- src/ru/slava/java/core/Main.java
+++ src/ru/slava/java/core/Main.java
@@ -1,8 +1,42 @@
 package ru.slava.java.core;
 
-public class Main {
+import java.io.IOException;
+import java.util.Arrays;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    void array() throws MyArraySizeException, MyArrayDataException {
+        String massiv = "1253785463254789";
+        for (int i = 0; i < massiv.length(); i++) {
+            if (massiv.charAt(i) < '1' || massiv.charAt(i) > '9')
+                throw new MyArrayDataException("symbol " + (i + 1) + " is not a number");
+        }
+        calculation(massiv);
+    }
+
+    void calculation(String massiv) throws MyArraySizeException, MyArrayDataException {
+        int len = 0;
+
+        if (massiv.length() != 16) throw new MyArraySizeException("matrix is not 4 x 4");
+
+        int sum = 0;
+        Integer[][] convMassiv = new Integer[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                convMassiv[i][j] = Integer.parseInt(String.valueOf(massiv.charAt(len)));
+                len++;
+                System.out.print(convMassiv[i][j] + " ");
+                sum = convMassiv[i][j] + sum;
+
+            }
+            System.out.println();
+        }
+        System.out.println("sum = " + sum);
+
+    }
+
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
+
+        Main ob = new Main();
+        ob.array();
     }
 }
---
+++ src/ru/slava/java/core/MyArrayDataException.java
@@ -0,0 +1,11 @@
+package ru.slava.java.core;
+
+public class MyArrayDataException extends NumberFormatException {
+
+
+        public MyArrayDataException(String message){
+
+        super(message);
+    }
+
+}
