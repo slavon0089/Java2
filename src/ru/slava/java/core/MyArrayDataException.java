8ff0020b12e7123c94cb20d33b2e82ff120b7cb1
Viacheslav_Shchukin <slavon0089@yandex.ru>
2021-05-19 16:44:46 +0300

    Init commit

---
+++ src/ru/slava/java/core/MyArrayDataException.java
@@ -0,0 +1,13 @@
+package ru.slava.java.core;
+
+public class MyArrayDataException extends Exception {
+
+
+    public MyArrayDataException(String message){
+
+        super(message);
+    }
+
+}
+
+
---
+++ src/ru/slava/java/core/MyArraySizeException.java
@@ -0,0 +1,10 @@
+package ru.slava.java.core;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message){
+
+        super(message);
+    }
+
+}
