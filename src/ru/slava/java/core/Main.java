aaef34f5dde34fcec3fe8373ea43daf47aa604bd
slavon0089 <slavon0089@yandex.ru>
2021-05-22 21:06:57 +0300

    HW3_1

--- src/ru/slava/java/core/Main.java
+++ src/ru/slava/java/core/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void changeArray (String array[]){
 
         String temp;
+        int a=0;
         for (int i = 0; i < array.length -1; i+=2) {
             temp = array[i];
             array[i] = array[i+1];
